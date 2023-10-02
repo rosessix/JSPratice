b76fdc72f53c67696d78b42e0bcb6459a4c45c42
rosessix <35843808+rosessix@users.noreply.github.com>
2023-10-02 23:56:05 +0200

    Fixed issues with the editor, made sure the output works, added tasks.

--- jstask/script.js
+++ jstask/script.js
@@ -1,29 +1,62 @@
 let editor = null;
+let currentTask = []
 const Tasks = [
     {
-        description: "This is a simple task. Make a function called addNumbers that take two paramterers, n1 and n2. These number should be added and returned.",
-        name: "addNumbers task",
-    }
+        description: `# Add two numbers. \nMake a function called \`addNumbers\` that take two paramterers, **n1** and **n2**. \n\nThese number should be added and returned.`,
+        name: "addNumbers() task",
+        template: `function addNumbers(n1,n2) {
+    // Place your code here!
+}`,
+        test: [5,5,10]
+    },
+    {
+        description: `# Subtract two numbers. \nMake a function called \`subtractNumbers\` that take two paramterers, **n1** and **n2**.  \n\nThese number should be subtracted from each other and returned.`,
+        name: "subtractNumbers() task",
+        test: [5,5,0]
+    },
 ]
 $(document).ready(() => {
     editor = CodeMirror(document.getElementById("editor"), {
         mode: "javascript",
         theme: 'monokai',
+        fontSize: "15pt",
         lineWrapping: true,
         lineNumbers: true,
         styleActiveLine: true,
         matchBrackets: true,
-        
-    })
+        autoCloseBrackets: true
+    });
 
-    $('#sTasks').html(``)
-    for (let i = 0; i < Tasks.length; i++) {
-        console.log('lol')
-        $('#sTasks').append(`<option value="saab">Saab</option>`)
-    }
-})
+    $('#sTask').html(``);
+    setTimeout(() => {
+        for (let i = 0; i < Tasks.length; i++) {
+            console.log('lol');
+            $('#sTask').append(`
+                <option value="${i}">${Tasks[i].name}</option>
+            `);
+        };
+
+        setTask({value: 0})
+    }, 500);
+});
 
 const runCode = () => {
     let code = editor.getValue();
-    console.log(code)
-}
+    let tests = currentTask.test
+    try {
+        var result = eval(`(${code}(${tests[0]}, ${tests[1]}))`)
+        $('#codeoutput').html(`Output: ${result}`)
+        if (result == tests[2]) {
+            $('#codeoutput').html("Test passed!")
+        }
+    } catch(err) {
+        $('#codeoutput').html(err)
+    }    
+};
+
+const setTask = (task) => {
+    currentTask = Tasks[task.value];
+    const mdText = marked.parse(currentTask.description);
+    $('#taskdescription').html(mdText);
+    editor.setValue(currentTask.template || "")
+};
